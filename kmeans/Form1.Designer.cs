e24e7866cbb06a2d9b0ef80874480720bbb48c7d
SasankaPM <pubudism@gmail.com>
2019-07-18 09:23:45 +0530

    first

---
+++ kmeans/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace kmeans
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.pnlP = new System.Windows.Forms.Panel();
+            this.btnRanGen = new System.Windows.Forms.Button();
+            this.txtNop = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.pnlP.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(26, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(527, 627);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.rb2);
+            this.groupBox1.Controls.Add(this.rb1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(575, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 287);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cordinates";
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rb2.Location = new System.Drawing.Point(6, 65);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(130, 20);
+            this.rb2.TabIndex = 2;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Select Clusters";
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.rb1.Location = new System.Drawing.Point(6, 39);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(117, 20);
+            this.rb1.TabIndex = 1;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Select Points";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Location = new System.Drawing.Point(14, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 160);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 466);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 466);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackgroundImage = global::kmeans.Properties.Resources.COL2;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnClear.Location = new System.Drawing.Point(614, 607);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(133, 41);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(569, 328);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(177, 201);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Clusters";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 26;
+            this.listBox2.Location = new System.Drawing.Point(12, 53);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(151, 134);
+            this.listBox2.TabIndex = 0;
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.Color.Maroon;
+            this.btnShow.BackgroundImage = global::kmeans.Properties.Resources.COL2;
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnShow.Location = new System.Drawing.Point(614, 535);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(132, 51);
+            this.btnShow.TabIndex = 6;
+            this.btnShow.Text = "SHOW";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pnlP
+            // 
+            this.pnlP.BackColor = System.Drawing.Color.Transparent;
+            this.pnlP.Controls.Add(this.btnRanGen);
+            this.pnlP.Controls.Add(this.txtNop);
+            this.pnlP.Enabled = false;
+            this.pnlP.Location = new System.Drawing.Point(784, 119);
+            this.pnlP.Name = "pnlP";
+            this.pnlP.Size = new System.Drawing.Size(115, 150);
+            this.pnlP.TabIndex = 7;
+            this.pnlP.Visible = false;
+            // 
+            // btnRanGen
+            // 
+            this.btnRanGen.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnRanGen.Location = new System.Drawing.Point(15, 108);
+            this.btnRanGen.Name = "btnRanGen";
+            this.btnRanGen.Size = new System.Drawing.Size(75, 32);
+            this.btnRanGen.TabIndex = 1;
+            this.btnRanGen.Text = "Draw";
+            this.btnRanGen.UseVisualStyleBackColor = false;
+            this.btnRanGen.Click += new System.EventHandler(this.btnRanGen_Click);
+            // 
+            // txtNop
+            // 
+            this.txtNop.Location = new System.Drawing.Point(15, 72);
+            this.txtNop.Name = "txtNop";
+            this.txtNop.Size = new System.Drawing.Size(75, 20);
+            this.txtNop.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::kmeans.Properties.Resources.im;
+            this.ClientSize = new System.Drawing.Size(934, 673);
+            this.Controls.Add(this.pnlP);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "KMeans Algorithem";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.pnlP.ResumeLayout(false);
+            this.pnlP.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.Panel pnlP;
+        private System.Windows.Forms.Button btnRanGen;
+        private System.Windows.Forms.TextBox txtNop;
+    }
+}
+
---
+++ kmeans/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kmeans
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int[,] xy = new int[10010,2];
+        public static int[,] clusters = new int[10, 2];
+        public static int[,] cluster2 = new int[10, 2];
+        public static int[] dis = new int[50];
+        public static int[] nocp = new int[50];
+        static int np = 0;
+        static int nc = 0;
+        static int mindisindex=0;
+        static int count = 0;
+        Random r = new Random();
+
+        Brush[] b = { Brushes.Chartreuse, Brushes.DeepPink, Brushes.MidnightBlue, Brushes.OrangeRed, Brushes.Gold, Brushes.Indigo, Brushes.DodgerBlue,Brushes.Lime,Brushes.MediumTurquoise, Brushes.Fuchsia};
+
+        public void drawpoint(int x, int y)
+        {
+            Graphics g = panel1.CreateGraphics();         
+            g.FillEllipse(Brushes.Black, x, y, 10, 10);                      
+        }
+
+        public void drawpoint2(int x, int y,Brush brush)
+        {
+            Graphics g = panel1.CreateGraphics();           
+            g.FillEllipse(brush,x,y, 10, 10);
+        }
+
+        public void drawpointw(int x, int y)
+        {
+            Graphics g = panel1.CreateGraphics();            
+            g.FillEllipse(Brushes.DimGray, x, y, 10, 10);
+        }
+        public void drawpointw2(int x, int y)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.FillRectangle(Brushes.DimGray, x, y, 10, 10);
+        }
+        public int distance(int x1, int y1, int x2, int y2)
+        {
+            double x = (double)(x1 - x2);
+            double y = (double)(y1 - y2);
+
+            return (int)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));           
+        }
+
+        public void eql()
+        {
+            for (int i = 0; i < nc; i++)
+            {
+                clusters[i, 0] = cluster2[i, 0]/nocp[i];
+                clusters[i, 1] = cluster2[i, 1]/nocp[i];
+                cluster2[i, 0] = 0;
+                cluster2[i, 1] = 0;
+            }
+            for (int i = 0; i < nc; i++)
+            {
+                nocp[i] = 0;
+            }
+        }
+           
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rb1.Checked)
+            {
+                xy[np, 0] = (int)e.X;
+                xy[np, 1] = (int)e.Y;
+                drawpoint(e.X, e.Y);
+                listBox1.Items.Add(e.X.ToString() + "\t" + (panel1.Height - e.Y).ToString());
+                np += 1;
+                if (np == 9999)
+                {
+                   // MessageBox.Show("one more to input");
+                }
+            }   
+            else if(rb2.Checked){
+
+                clusters[nc, 0] = (int)e.X;
+                clusters[nc, 1] = (int)e.Y;
+                // drawpoint(e.X, e.Y);
+                //int N = xy.Count;
+                xy[np, 0] = clusters[nc, 0];
+                xy[np, 1] = clusters[nc, 1];
+                np++;
+                Graphics g = panel1.CreateGraphics();
+                g.FillRectangle(b[nc], clusters[nc, 0], clusters[nc, 1], 10, 10);
+                listBox2.Items.Add(e.X.ToString() + "\t" + (panel1.Height - e.Y).ToString());
+                nc += 1;
+                if (nc == 9)
+                {
+                    MessageBox.Show("one more cluster to input");
+                }
+            }                   
+        }
+
+        public void show()
+        {
+            label1.Text = xy[np - 1, 0].ToString();
+            label2.Text = xy[np - 1, 1].ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //label2.Text = listBox1.GetItemText(listBox1.SelectedItem);
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox1.Items.IndexOf(listBox1.SelectedItem);           
+            clusters[nc, 0] = xy[index, 0];
+            clusters[nc, 1] = xy[index, 1];
+
+            listBox2.Items.Add(clusters[nc, 0] + "\t" + (clusters[nc, 1]-panel1.Height));
+            nc++;          
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.DimGray);
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        public void calculate()
+        {
+            for (int i = 0; i < np; i++)
+            {
+                for (int j = 0; j < nc; j++)
+                {
+                    dis[j] = distance(xy[i, 0], xy[i, 1], clusters[j, 0], clusters[j, 1]);                   
+                }
+
+                mindisindex = 0;
+                for (int k = 0; k < nc - 1; k++)
+                {
+                    if (dis[k + 1] <= dis[mindisindex])
+                    {
+                        mindisindex = k + 1;
+                    }                  
+                }                
+                drawpoint2(xy[i, 0], xy[i, 1], b[mindisindex]);
+                cluster2[mindisindex, 0] += xy[i, 0];
+                cluster2[mindisindex, 1] += xy[i, 1];
+                nocp[mindisindex] += 1;                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {        
+            if (count == 0)
+            {
+                calculate();
+                for (int i = 0; i < nc; i++)
+                {                   
+                    Graphics g = panel1.CreateGraphics();
+                    g.FillRectangle(b[i], cluster2[i, 0] / nocp[i], cluster2[i, 1] / nocp[i], 10, 10);
+                    
+                    drawpointw2(clusters[i, 0], clusters[i, 1]);
+                    g.FillEllipse(b[i], clusters[i, 0], clusters[i, 1], 10, 10);
+
+                    listBox2.Items.Clear();                   
+                }
+                count++;
+            }
+            else
+            {
+                for (int i = 0; i < nc; i++) {               
+                    Graphics g = panel1.CreateGraphics();
+                    g.FillRectangle(Brushes.DimGray, cluster2[i, 0] / nocp[i], cluster2[i, 1] / nocp[i], 10, 10);
+                }
+                eql();
+                calculate();
+                for (int i = 0; i < nc; i++)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    g.FillRectangle(b[i], cluster2[i, 0] / nocp[i], cluster2[i, 1] / nocp[i], 10, 10);
+                }
+            }                        
+        }
+
+        private int randy()
+        {         
+            return r.Next(0, 620);
+        }
+
+        private int randx()
+        {
+            return r.Next(0, 520);
+        }
+        private void btnRanGen_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(txtNop.Text);
+            for(int i=0;i<n;i++)
+            {
+                int x = randx();
+                int y = randy();
+                //int y = 20;
+                drawpoint(x, y);
+                xy[np, 0] = x;
+                xy[np, 1] = y;
+                listBox1.Items.Add(x.ToString() + "\t" + y.ToString());
+                np += 1;
+                if (np == 999)
+                {
+                    MessageBox.Show("one more to input");
+                }
+            }
+        }
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb1.Checked)
+            {
+                string ms1 = "Do you wish to genarate points automatically?(<10,000)";
+                string ms2 = "Generate Points";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                //DialogResult result = MessageBox.Show(ms1, ms2, buttons);
+                if (MessageBox.Show(ms1, ms2, buttons) == DialogResult.Yes)
+                {
+                    pnlP.Visible = true;
+                    pnlP.Enabled = true;
+                   //rb1.Enabled = false;
+                    rb1.Checked = false;
+                }
+            }
+           
+        }
+
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("You can enter 10 points as clusters (^.^)");
+            pnlP.Visible = false;
+        }
+    }
+}
